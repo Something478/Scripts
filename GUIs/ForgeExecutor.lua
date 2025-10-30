9eeaa115267f721b33851a53083977ab4dcee15d
StarFlow <m66361544@gmail.com>
2025-10-30 21:10:37 +0200

    update GUIs/ForgeExecutor.lua

--- GUIs/ForgeExecutor.lua
+++ GUIs/ForgeExecutor.lua
@@ -1,2 +1,2 @@
---[[========> Moved
+--[[========> Moved to diff file ]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/Scripts/refs/heads/main/GUIs/Forge%20executor.lua"))()
